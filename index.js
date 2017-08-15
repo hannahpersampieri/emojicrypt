073d6df81ed316409222c7236143a162e748858f
Hannah <hannahvoelker13@gmail.com>
2017-08-15 13:36:36 -0700

    making this into a module

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var base64 = require('base64-min');
 var emoji = require('emojify.js');
 
-function encrypt(text) {
+exports.encrypt = function(text) {
    var cipherText = base64.encode(text);
    var emojiCipherText = emoji.emojify(cipherText);
    return emojiCipherText;
 }
 
-function decrypt(emojiText) {
+exports.decrypt = function(emojiText) {
   var cipherText = emoji.unemojify(emojiText);
   var plainText = base64.decode(cipherText);
   return plainText;
