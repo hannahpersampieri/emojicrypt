6507b6f5b1363f7b59c9f79adbf543bf7900deaf
Hannah Voelker <hannahvoelker13@gmail.com>
2017-08-20 19:25:08 -0700

    finalizing

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 var base64 = require('base64-min');
+var utf8 = require('utf8');
 var emoji = require('emojify.js');
 
+
 exports.encrypt = function(text) {
+   var text = utf8.encode(text);
    var cipherText = base64.encode(text);
    var emojiCipherText = emoji.emojify(cipherText);
    return emojiCipherText;
@@ -10,5 +13,6 @@ exports.encrypt = function(text) {
 exports.decrypt = function(emojiText) {
   var cipherText = emoji.unemojify(emojiText);
   var plainText = base64.decode(cipherText);
+  plaintext = utf8.decode(plaintext);
   return plainText;
 }
